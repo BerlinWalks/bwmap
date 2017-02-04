865a12a609425b665e57b3b98289576d9f8e783d
Joachim Kuebart <joachim.kuebart@gmail.com>
2017-02-04 18:10:53 +0100

    Fix piping input to pbftiles.
    Can I just point out that it grieves me deeply when Node.js claims that
    
    	File I/O is provided by simple wrappers around standard POSIX
    	functions.
    
    And yet the functions don't behave *at all* according to 40 years worth of
    expectation. The fact that fs.readFileSync() throws EPIPE when standard
    input isn't connected to a regular file is just… sad :'(

--- util/pbftiles.js
+++ util/pbftiles.js
@@ -32,9 +32,10 @@
 
 const fs = require('fs');
 const geojsonVt = require('geojson-vt');
+const rw = require('rw');
 const vtpbf = require('vt-pbf');
 
-const gj = JSON.parse(fs.readFileSync(process.stdin.fd, 'utf8'));
+const gj = JSON.parse(rw.readFileSync('/dev/stdin', 'utf8'));
 const tileindex = geojsonVt(gj, { 'indexMaxZoom': 13, 'indexMaxPoints': 0 });
 
 for (let { z, x, y } of tileindex.tileCoords) {
