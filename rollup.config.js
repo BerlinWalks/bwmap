1e21670ad4c46fd261d9c324407a16ed91d05964
Joachim Kuebart <joachim.kuebart@gmail.com>
2017-02-06 20:52:36 +0100

    Minify output using UglifyJS.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import commonJs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
         entry:                  'js/gpxmap.js',
         format:                 'amd',
         dest:                   'dist/gpxmap.js',
+        sourceMap:              true,
         external: [
                                 'leaflet',
                                 'require',
@@ -12,5 +14,6 @@ export default {
         plugins: [
                                 commonJs(),
                                 nodeResolve(),
+                                uglify(),
         ],
 }
